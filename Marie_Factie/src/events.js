b4ef7e4beed636e9c0cd028b93b87a79b7bf16c2
Lokoroga <leidyokoro@gmail.com>
2025-01-15 20:20:27 +0100

    propuesta Mile

---
+++ Marie_Factie/src/events.js
@@ -0,0 +1,12 @@
+import { PlayGame } from './uiDom'
+
+document.getElementById('buttonStart').addEventListener('click' , () => {
+
+    PlayGame();
+})
+
+document.getElementById('buttonRefresh').addEventListener('click' , () => {
+
+    PlayGame();
+})
+
